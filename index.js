9f71338123d0c3b76513fa7a8893fd14399d8070
prateekgrewal123 <Prateekgrewal123@gmail.com>
2023-11-20 17:41:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 2,
+    spaceBetween: 5,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
